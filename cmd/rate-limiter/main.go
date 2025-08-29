c226272c823ee53fe27fbb87d9c6d766b99a5448
Ken Utsunomiya <kenutsunomiya08@gmail.com>
2025-08-30 05:35:21 +0900

    feat: start gRPC server

---
+++ cmd/rate-limiter/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"cmp"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	_ "github.com/KenUtsunomiya/my-rate-limiter/proto/ratelimit/v1"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	if err := (func() error {
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+		addr := ":" + cmp.Or(os.Getenv("GRPC_SERVER_PORT"), "50051")
+		lis, err := net.Listen("tcp", addr)
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+
+		log.Printf("listening on port %s", addr)
+
+		server := grpc.NewServer()
+
+		go func() {
+			<-sigCh
+			log.Println("shutting down gRPC server...")
+			server.GracefulStop()
+		}()
+
+		if err = server.Serve(lis); err != nil {
+			log.Fatalf("failed to start gRPC server: %v", err)
+		}
+
+		return nil
+	})(); err != nil {
+		log.Fatalf("unknown error occurred: %v", err)
+	}
+}
