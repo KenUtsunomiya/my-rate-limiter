5ba4cb5f282607ec85f6dfd7c7a7b81f6fb23c0d
Ken Utsunomiya <kenutsunomiya08@gmail.com>
2025-09-13 23:29:07 +0900

    create ratelimit package

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -9,17 +9,24 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	pb "github.com/KenUtsunomiya/my-rate-limiter/pb/ratelimit/v1"
+	"github.com/KenUtsunomiya/my-rate-limiter/pkg/ratelimit"
 	"github.com/KenUtsunomiya/my-rate-limiter/pkg/valkey"
 )
+
+type rateLimiter interface {
+	Allow(ctx context.Context, userID string, method string, resource string) (bool, error)
+}
 
 type Server struct {
 	pb.UnimplementedRateLimiterServer
 	vkClient valkey.Client
+	rl       rateLimiter
 }
 
 func NewServer(client valkey.Client) *Server {
 	return &Server{
 		vkClient: client,
+		rl:       ratelimit.NewRateLimiter(client),
 	}
 }
 
@@ -33,8 +40,7 @@ func (s *Server) Hello(ctx context.Context, _ *emptypb.Empty) (*emptypb.Empty, e
 func (s *Server) Check(ctx context.Context, req *pb.RateLimitRequest) (*pb.RateLimitResponse, error) {
 	log.Printf("check rate limit: user_id=%s, method=%s, resource=%s", req.UserId, req.Method, req.Resource)
 
-	rl := newInMemoryRateLimiter()
-	allowed, err := rl.allow(ctx, req.UserId, req.Method, req.Resource)
+	allowed, err := s.rl.Allow(ctx, req.UserId, req.Method, req.Resource)
 	if err != nil {
 		return &pb.RateLimitResponse{
 			Allowed: false,
